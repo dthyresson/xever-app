1c12a424a8aaff3b52e972018294b128df81b3c2
A. David Thyresson <david@sportsilab.com>
2020-06-24 15:58:08 -0400

    Format story with tags and date

--- web/src/components/StoriesCell/StoriesCell.js
+++ web/src/components/StoriesCell/StoriesCell.js
@@ -1,16 +1,30 @@
-import { Box, Image, Link, Stack, Text } from '@chakra-ui/core'
+import {
+  Box,
+  Flex,
+  Heading,
+  Icon,
+  Image,
+  Link,
+  Stack,
+  Tag,
+  TagLabel,
+  Text,
+  Tooltip,
+} from '@chakra-ui/core'
 import moment from 'moment'
 
 export const QUERY = gql`
   query {
     stories {
       channelEmojiIcon
+      channelName
       feedlyId
       imageUrl
       url
       summarySentences
       title
       publishedAt
+      topicLabels
     }
   }
 `
@@ -29,30 +43,75 @@ const renderSentences = ({ story }) => {
   ))
 }
 
+const renderTopicLabels = ({ story }) => {
+  return story?.topicLabels?.map((topicLabel, index) => (
+    <Tag
+      size="sm"
+      variantColor="gray"
+      variant="subtle"
+      m={1}
+      key={`${story.feedlyId}-topic-${index}`}
+    >
+      <TagLabel>{topicLabel}</TagLabel>
+    </Tag>
+  ))
+}
+
 const renderStories = ({ stories }) => {
   return stories.map((story) => (
-    <Box key={story.id} w="80%">
-      <Box borderWidth="1px" rounded="lg" p={4}>
+    <Box key={story.id} w="50%">
+      <Box
+        borderWidth="1px"
+        rounded="lg"
+        p={4}
+        boxShadow="0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);"
+      >
         <Image
           src={story.imageUrl}
           alt={story.title}
-          size="240px"
+          size="100%"
+          maxH="240px"
           objectFit="cover"
-          fallbackSrc="https://via.placeholder.com/240"
+          fallbackSrc="https://via.placeholder.com/120"
           rounded="lg"
           mb={4}
         />
-        <Text fontSize="xl" mb={8}>
-          {story.channelEmojiIcon}{' '}
+        <Heading as="h3" size="lg" fontWeight="bold" mb={8}>
           <Link href={story.url} isExternal>
             {story.title}
           </Link>
-        </Text>
+        </Heading>
         {story.summarySentences &&
           story.summarySentences.length > 0 &&
           story.summarySentences[0] &&
           renderSentences({ story })}
-        <Text>{moment(story.publishedAt).fromNow()}</Text>
+        <Flex
+          mb={4}
+          spacing={2}
+          wrap="wrap"
+          align="space-between"
+          justify="center"
+        >
+          {renderTopicLabels({ story })}
+        </Flex>
+        <Flex justify="space-between">
+          <Flex align="center">
+            <Tooltip label={story.channelName} hasArrow>
+              <Text>{story.channelEmojiIcon}</Text>
+            </Tooltip>
+          </Flex>
+          <Flex align="center">
+            <Icon name="time" size="13px" mr={1} color="black" />
+            <Tooltip
+              label={moment(story.publishedAt).format(
+                'dddd, MMMM Do YYYY, h:mm a'
+              )}
+              hasArrow
+            >
+              <Text fontSize="sm">{moment(story.publishedAt).fromNow()}</Text>
+            </Tooltip>
+          </Flex>
+        </Flex>
       </Box>
     </Box>
   ))
--- web/src/pages/StoriesPage/StoriesPage.js
+++ web/src/pages/StoriesPage/StoriesPage.js
@@ -1,11 +1,9 @@
 import AppLayout from 'src/layouts/AppLayout'
-import { Heading } from '@chakra-ui/core'
 import StoriesCell from 'src/components/StoriesCell'
 
 const StoriesPage = () => {
   return (
     <AppLayout>
-      <Heading>Stories</Heading>
       <StoriesCell />
     </AppLayout>
   )
